40571e202eabd99feaaa04274c7c8e1be1aafff4
CuT4 <3222helicopter@protonmail.com>
2022-12-19 23:50:19 +0100

    19/12/2022 (js-master)

---
+++ JAVASCRIPT/3-MASTER/prototipos.js
@@ -0,0 +1,8 @@
+// 13:02 master JS
+let variable="un string";
+let funcion=function(){}
+
+console.log(variable.__proto__) // '.__proto__' es para prototipos heredados 
+console.log(funcion.prototype) // '.prototype' es para prototipos creados (funciones) 
+console.log(funcion)
+console.log(variable)
