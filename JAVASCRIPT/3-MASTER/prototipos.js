21b370a6af6de6cfb4a96df41714d07ab8d5301c
CuT4 <3222helicopter@protonmail.com>
2022-12-21 00:20:05 +0100

    20/12/2022

--- JAVASCRIPT/3-MASTER/prototipos.js
+++ JAVASCRIPT/3-MASTER/prototipos.js
@@ -1,8 +1,20 @@
-// 13:02 master JS
-let variable="un string";
-let funcion=function(){}
+class Objeto{
+    saludar(){
+        console.log("hola")
+    }
+    despedirse(){
+        console.log("hasta luego")
+    }
+}
+let variable=["un string", 234, null, "patito"];//Todo en JS es interpretado como objeto, con prototipos predeterminados excepto las funciones.
+let funcion=function(){}; //Las funciones crean nuevos prototipos propios.
+let objeto= new Objeto();
 
-console.log(variable.__proto__) // '.__proto__' es para prototipos heredados 
-console.log(funcion.prototype) // '.prototype' es para prototipos creados (funciones) 
-console.log(funcion)
-console.log(variable)
+console.log(variable);
+console.log(variable.__proto__);
+
+console.log(objeto);
+console.log(objeto.__proto__);
+console.log(objeto.__proto__.__proto__);
+
+
