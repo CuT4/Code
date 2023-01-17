8ad2fe9647b5ae345a2725786a20f33036952444
CuT4 <3222helicopter@protonmail.com>
2023-01-18 00:03:24 +0100

    18/01/2023

--- ARDUINO/joystick/joystickPython/joystickPy2.py
+++ ARDUINO/joystick/joystickPython/joystickPy2.py
@@ -1,34 +1,28 @@
-"""
-El problema aparece al utilizar pyautogui, debo de cambiar
-la librería para mover el cursor y ver si funciona entonces.
-"""
 
 import serial
 import pyautogui
 import time
+#I import the libraries.
+arduino_port="/dev/ttyACM0" #Serial port for comunication(ubuntu).
+baud=9600#frecuency
 
-arduino_port="/dev/ttyACM0"
-baud=9600
-#in the code I've readen, they took anly 10 samples but I'm gonna collect data till the program is closed.
-ser=serial.Serial(arduino_port, baud)
-print("Connected to the arduino port at ", baud, "bauds")
-# screenWidth, screenHeight=pyautogui.size()
-# mouseX, mouseY=pyautogui.position() #actual positon X and y of the mouse
-# screenSize=[screenWidth, screenHeight]# is [1920, 1080] in my monitor
+ser=serial.Serial(arduino_port, baud)# Connect to /dev/ttyACM0 at 9600 bauds
+print("Connected to the arduino port at ", baud, "bauds") # Confirm connection
 
-
-
-while True:
-    data=ser.readline().decode('utf-8').rstrip()
-    xyValues=data.split(",")
-    final_xyValues=[]
+while True:#I'm using a while because I want it to run while the program is running, forever.
+    data=ser.readline().decode('utf-8').rstrip() # Decode the data printed by arduino in the serial port.
+    xyValues=data.split(",")# creates an array like this: ['x','y']
+    final_xyValues=[]# declaration of a new array
     for i in xyValues:
         final_xyValues.append(int(i))
+    #When the loop has finished, I have this array: [x, y], where x and y are integers.
     print(final_xyValues)
-    #to check if it works comment the code below:
-    # if len(final_xyValues)==2:
-    #     pyautogui.moveRel(final_xyValues[0], final_xyValues[1])
-    #     time.sleep(0.2)
+    #to check the problem uncomment the code below:
+    """
+    if len(final_xyValues)==2:
+        pyautogui.moveRel(final_xyValues[0], final_xyValues[1])
+        time.sleep(0.2)
+    """
 
 """
 Here is where I need help. I want to move the cursor with the values that
@@ -40,11 +34,14 @@ Take in count that the variable data is a string like this: '405,678', wich is
 transform to an array with two integers as values. Now, you must know that the values
 of final_xyValues are from an analog input of a joystick connected to an Arduino Uno
 and readen from the serial port. I want to move the cursor with the analog inputs of the
-joystick
+joystick.
 
+ISSUE: when I uncomment the code from line 21 to 25 the first values of the joystick x and y
+position in that moment are ok but after that the data continues identical to the first values
+read. The values are correctly and constantly been updated if I comment this part of the code,
+if I move the joystick the data changes.
 """
     
 
 
    
-    
